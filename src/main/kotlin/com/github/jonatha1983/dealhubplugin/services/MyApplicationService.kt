6452a964d1261f9ef0348af5c4170b94a4d8d9d2
GitHub Action <action@github.com>
2022-09-01 09:38:47 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonatha1983/dealhubplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jonatha1983/dealhubplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonatha1983.dealhubplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonatha1983.dealhubplugin.MyBundle
 
 class MyApplicationService {
 
